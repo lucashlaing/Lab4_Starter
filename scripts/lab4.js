3c02995c0b6348ee21ee4c197e376abcbb2308d9
lucashlaing <mhlaing@ucsd.edu>
2025-04-27 21:15:24 -0700

    fixed price

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -30,7 +30,7 @@ function discountPrices(prices, discount) {
     if(length == 0) return false;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
