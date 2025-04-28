6f912b8600912296f703d9c7bf00009032273145
lucashlaing <mhlaing@ucsd.edu>
2025-04-27 21:11:32 -0700

    fixed small bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,7 +7,7 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
@@ -27,6 +27,7 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
+    if(length == 0) return false;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
         discountedPrice += prices[i] * (1 - discount);
